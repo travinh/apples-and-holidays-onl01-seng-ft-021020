35d7f19141be4eb7b169391cb5621a2544b47fc9
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-18 06:15:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
         string_array = temp_holiday.split("_")
         i=0 
         while i<string_array.length do
-          string_array[i] << string_array[i].capitalize
+          string_array[i] = string_array[i].capitalize
           i+=1
         end
         puts string_array
